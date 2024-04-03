a4f91edf761dbeb37e4265d295e41ed49dd1853b
neamul-fahim <102896638+neamul-fahim@users.noreply.github.com>
2024-04-03 23:38:16 +0600

    admin site User Creation

--- user_management/admin.py
+++ user_management/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-admin.site.register(CustomUser)
+
+
+class CustomUserAdmin(UserAdmin):
+    # Add the fields you want to display
+    model = CustomUser
+    list_display = ('email', 'name', 'is_active',
+                    'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'groups', 'user_permissions')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_staff',
+         'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
